1b87bc53d5e6367479db15162e5e03e090d433e1
neocodered <nadilhansith@gmail.com>
2025-01-16 21:25:26 +0800

    Refactor GPS controller: replace old implementation with a new version and remove unused project files

--- controllers/my_gps/build/release/my_gps.d
+++
@@ -1,5 +0,0 @@
-build/release/my_gps.o: my_gps.cpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/GPS.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Program\ Files/Webots/include/controller/c/webots/types.h
