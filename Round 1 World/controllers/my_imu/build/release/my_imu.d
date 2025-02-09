c2c95f1dd75e68a9ef3b0588590b778aec32dba2
neocodered <nadilhansith@gmail.com>
2025-02-09 11:24:48 +0800

    task document

---
+++ Round 1 World/controllers/my_imu/build/release/my_imu.d
@@ -0,0 +1,5 @@
+build/release/my_imu.o: my_imu.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/InertialUnit.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h
