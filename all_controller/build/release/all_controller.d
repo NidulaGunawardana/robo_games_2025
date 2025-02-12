570e981a4277ace330b8e481893b86b5d3237767
Nadil Gunawardane <104412766+neocodered@users.noreply.github.com>
2025-02-13 00:13:56 +0800

    Add files via upload

---
+++ all_controller/build/release/all_controller.d
@@ -0,0 +1,10 @@
+build/release/all_controller.o: all_controller.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Keyboard.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Camera.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Display.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/ImageRef.hpp
