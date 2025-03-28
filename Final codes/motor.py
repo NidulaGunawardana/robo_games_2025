3c76e433ee32d19659b9e612de876313866392dc
Nidula Gunawardana <nidulam@outlook.com>
2025-03-28 21:37:36 +0100

    Real world code refined

--- Final codes/motor.py
+++ Final codes/motor.py
@@ -1,32 +1,37 @@
 import time
-# import KobukiDriver as kd
+import KobukiDriver as kd
 
 class MotorController:
     
-    def __init__(self, keyboard_control=False):
+    def __init__(self,keyboard_control=False):
         self.keyboard_control = keyboard_control
-        # self.my_kobuki = kd.Kobuki()
-        print("Initialized MotorController and played startup sound.")
+        self.my_kobuki = kd.Kobuki()
+        self.my_kobuki.play_on_sound()
     
     def get_encoder(self):
         """Get encoder data from the robot."""
-        print("Called get_encoder function.")
-        return 0, 0
+        encoder_data = self.my_kobuki.encoder_data()
+        print(encoder_data)
+        return encoder_data[0], encoder_data[1]
         
     def move_forward(self):
-        print("Called move_forward function.")
+        self.my_kobuki.move(200, 200, 0)
 
+    
     def move_backward(self):
-        print("Called move_backward function.")
+        self.my_kobuki.move(-200, -200, 0)
 
+        
     def turn_left(self):
-        print("Called turn_left function.")
+        self.my_kobuki.move(100, -100, 1)
 
+        
     def turn_right(self):
-        print("Called turn_right function.")
+        self.my_kobuki.move(-100, 100, 1)
+
 
     def stop(self):
-        print("Called stop function.")
+        self.my_kobuki.move(0, 0, 0)
         
     def keyboard_move(self):
         # Play start up sound
