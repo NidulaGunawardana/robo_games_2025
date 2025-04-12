de03ed188a0f5294a090125bd37ac5acfc15818c
neocodered <nadilhansith@gmail.com>
2025-04-12 14:44:45 +0800

    Refactor code structure for improved readability and maintainability

---
+++ Round 2/second_round_world_2/controllers/my_gps/build/release/my_gps.d
@@ -0,0 +1,14 @@
+build/release/my_gps.o: my_gps.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/GPS.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Gyro.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/InertialUnit.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Camera.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Display.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/ImageRef.hpp
